104a96301132c83826bc709167efb62a2431a6e0
Mahsa Behmanesh <mahsa.behmanesh74@gmail.com>
2024-10-02 14:08:13 +0330

    feat (test) : add test case for Insertion Sort

--- src/sort/insertion/insertion.test.js
+++ src/sort/insertion/insertion.test.js
@@ -1,24 +1,27 @@
-const { insertionSort } = require("../../../dist/insertion");
+const { InsertionSort } = require("../../../dist/sort/insertion/insertion");
 
 describe("Test cases for insertion sort", function () {
-  test("#1 test case", function () {
+  test("#1 test case increasing order : ", function () {
     const arr = [1, 4, 8, 2, 3, 7];
     const arr2 = [...arr];
-    const result = insertionSort(arr);
+    const result = InsertionSort(arr);
+    console.log("result #1 :", result);
     arr2.sort();
     expect(result).toEqual(arr2);
   });
 
-  test("#2 test my function against lodash", function () {
+  test("#2 test case decreasing order : ", function () {
     const arr = [1, 4, 8, 2, 3, 7];
-
-    const result = insertionSort(arr);
-    console.log(result);
+    const arr2 = [...arr];
+    arr2.sort().reverse();
+    const result = InsertionSort(arr, true);
+    console.log("result #1 :", result);
+    expect(result).toEqual(arr2);
   });
-});
 
-describe("Test cases for merge sort", function () {
-  test("#1 test for merge sort", function () {
-    console.log("----");
+  test("#3 test my function against lodash", function () {
+    const arr = [1, 4, 8, 2, 3, 7];
+    const result = InsertionSort(arr);
+    console.log("result #3 :", result);
   });
 });
--- src/sort/insertion/insertion.ts
+++ src/sort/insertion/insertion.ts
@@ -1,8 +1,11 @@
-export function insertionSort(arr: Array<number>): Array<number> {
+export function InsertionSort(
+  arr: Array<number>,
+  decreasing: boolean = false
+): Array<number> {
   for (let i = 0; i < arr.length; i++) {
     let key = arr[i];
     let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
+    while (j >= 0 && decreasing ? arr[j] < key : arr[j] > key) {
       arr[j + 1] = arr[j];
       j = j - 1;
     }
